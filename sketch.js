c2e262c24c0767273362af2e081d4f651a795491
Rileyk2006 <smigui2006@gmail.com>
2025-10-01 17:22:08 -0400

    reset

--- sketch.js
+++ sketch.js
@@ -10,22 +10,5 @@ function setup() {
 
 function draw() {
   background(220);
-  if (noMobileSensorInput()) return; // Check if sensor data is available
   image(screenshot,0,0,screenshot.width/3,screenshot.height/3);
-  test = DeviceOrientationEvent
-  text(test,50,50);
-}
-
-function setupMobile() {
-  if (typeof DeviceOrientationEvent !== "undefined") {
-    DeviceOrientationEvent.requestPermission()
-      .then(response => {
-        if (response === 'granted') {
-          // Permission granted, allow sensor access
-          console.log('Permission granted for mobile sensors');
-        } else {
-          console.log('Permission denied for mobile sensors');
-        }
-      });
-  }
 }
