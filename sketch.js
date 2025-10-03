9397b43c6db1de560b39a8b05d720c29625e82c0
Rileyk2006 <smigui2006@gmail.com>
2025-10-03 17:08:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function draw() {
     push();
     translate(windowWidth/2, windowHeight/2);
     rotate (-orientationY);
-    image(0-screenshot.width/6 0-screenshot.height/6,screenshot.width/3,screenshot.height/3);
+    image(0-screenshot.width/6,0-screenshot.height/6,screenshot.width/3,screenshot.height/3);
     pop();
 
     textSize(25);
